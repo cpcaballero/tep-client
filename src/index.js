7df383c1a4f886ce8ea5edd4829bac17193501e9
Carlos Caballero <carlospcaballero@outlook.com>
2020-02-08 10:48:34 +0800

    few changes, added images

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-dates/initialize';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
