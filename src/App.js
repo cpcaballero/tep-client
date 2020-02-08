b3912b18d39af321a290cbab50b2a3896035153b
ccruz <Charmaine.Cruz@asurion.com>
2020-02-08 15:39:47 +0800

    added sample card and image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/Landing';
 import Dashboard from './components/Dashboard';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card'
+
+import SampleImage from './assets/icons/sample.PNG';
 
 const App = () => {
 
@@ -29,6 +33,48 @@ const App = () => {
                             handleClose={handleClose}
                             handleShow={handleShow} /> 
                     } />
+                    <CardDeck>
+  <Card style={{marginTop: "20px"}}> 
+    <Card.Img variant="top" src={SampleImage} />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>
                     <Route exact path="/dashboard" render={
                         (props) => <Dashboard />
                     
